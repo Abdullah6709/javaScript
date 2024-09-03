bddf49cafdfe6b93f0ebf6c09e37924a413aa1cf
Abdullah6709 <101113739+Abdullah6709@users.noreply.github.com>
2024-09-03 11:14:13 +0000

    Data type summery

---
+++ 01_Basics/dataTypSummery.js
@@ -0,0 +1,24 @@
+const id = Symbol('123')
+const id2 = Symbol('123')
+console.log(id === id2);
+ 
+
+const bigNumber = 954632584568523n;
+
+//Array
+const heros = ["Iron Man","Thor","Wnada"];
+
+//Object
+
+let myObj = {
+    name: "Sajid",
+    age:22
+}
+
+//Functions
+
+const myFunction = function(){
+    console.log("Hello Function");
+    
+
+}
