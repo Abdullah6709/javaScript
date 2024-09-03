c16639e67a6e4595fca894fedbf2cfa6408da37c
Abdullah6709 <101113739+Abdullah6709@users.noreply.github.com>
2024-09-03 09:13:13 +0000

    Convertion of data types

---
+++ 01_Basics/conOperation.js
@@ -0,0 +1,25 @@
+let score = "54"
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+
+// 55 => 55
+// 55abc => NaN
+
+
+let isLogIn = 1
+
+let booleanInLogIn = Boolean(isLogIn)
+console.log(booleanInLogIn);
+// true =>  1; false =>  0;
+// "" => false
+// "Abdullah" => true
+
+let number = 33
+let stringNumber = String(number)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
